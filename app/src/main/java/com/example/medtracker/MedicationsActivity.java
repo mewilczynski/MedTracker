f39cd78ab19b0900f3503c67d881747fc8e50053
Loiy Habhab <loayhabhab1999@gmail.com>
2020-03-09 13:19:21 -0400

    All layout changes and files.

---
+++ app/src/main/java/com/example/medtracker/MedicationsActivity.java
@@ -0,0 +1,27 @@
+package com.example.medtracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MedicationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_medications);
+
+        Button button = (Button) findViewById(R.id.add_med);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddMedActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
