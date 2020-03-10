b6d1a161af637cbb261008175447a8815b8bba1d
Marta <32047132+mewilczynski@users.noreply.github.com>
2020-03-09 23:16:22 -0400

    adding a note changes the database document "0"

--- app/src/main/java/com/example/medtracker/AddNoteActivity.java
+++ app/src/main/java/com/example/medtracker/AddNoteActivity.java
@@ -2,15 +2,31 @@ package com.example.medtracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.icu.util.LocaleData;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class AddNoteActivity extends AppCompatActivity {
 
+    private FirebaseFirestore mDatabase;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mDatabase = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
         Button button = (Button) findViewById(R.id.add_sym_btn_sym);
@@ -19,7 +35,43 @@ public class AddNoteActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
                 startActivity(intent);
+                writeNewNote();
             }
         });
+    }
+
+    public class Note {
+
+        public String note;
+        public String today;
+        public int uid;
+
+        public Note() {
+            // Default constructor required for calls to DataSnapshot.getValue(User.class)
+        }
+
+        public Note(String note, String today, int uid) {
+            this.note = note;
+            this.today = today;
+            this.uid = uid;
+        }
+
+    }
+
+
+    private void writeNewNote() {
+
+        CollectionReference notes = mDatabase.collection("notes");
+        EditText editTxtNote = (EditText) findViewById(R.id.editNoteText);
+        String note = editTxtNote.getText().toString();
+
+        EditText editTxtDate = (EditText) findViewById(R.id.editDateText);
+        String date = editTxtDate.getText().toString();
+
+        int uid = 0;
+
+        Note newNote = new Note(note, date, uid);
+
+        notes.document("0").set(newNote);
     }
 }
--- app/src/main/java/com/example/medtracker/NotesActivity.java
+++ app/src/main/java/com/example/medtracker/NotesActivity.java
@@ -6,21 +6,52 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
 public class NotesActivity extends AppCompatActivity {
 
+    private FirebaseFirestore mDatabase;
+    private Query mQuery;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mDatabase = FirebaseFirestore.getInstance();
+
+        mQuery = mDatabase.collection("notes");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
-
         Button button = (Button) findViewById(R.id.add_note_btn);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), AddNoteActivity.class);
                 startActivity(intent);
+                GetData();
             }
         });
+    }
+
+    private void GetData()
+    {
+        DocumentReference mQuery = mDatabase.collection("notes").document("0");
+
+        String note = mQuery.collection("note").toString();
+        String date = mQuery.collection("today").toString();
+
+        TextView disTxtDate = (TextView) findViewById(R.id.textView24);
+        String disText = "- " + date;
+        disTxtDate.setText(disText);
+
+        TextView disTxtNote = (TextView) findViewById(R.id.textView24);
+        disText = "• " + note;
+        disTxtNote.setText(disText);
+
+
     }
 }
